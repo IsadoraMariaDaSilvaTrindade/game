35d152246a2b9028084ce5c6d61ff2ccd96542b7
JamileDelmondes <159553148+IsadoraMariaDaSilvaTrindade@users.noreply.github.com>
2024-05-22 11:46:44 -0300

    NADA
    22/05/24 1.0

--- redbool/scr/screen/index.tsx
+++ redbool/scr/screen/index.tsx
@@ -1,7 +1,7 @@
 import { View,Text,Image,} from 'react-native';
 import { styles } from './styles';
 import { useState } from 'react';
-
+import imagem from '../'
 export function SignIn(){
      const[text, setText] = useState("Escreva alguma coisa:  ")
 
